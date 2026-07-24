98ecf9bc20477f4922e8168cfe9ce8d40de8b28e
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-07-25 04:44:06 +0530

    Time: 2 ms (70.61%), Space: 56 MB (6.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,32 +1,50 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int count = 0;
-        int maxCount = 0;
-        int max = nums[0];
-        int i = 0;
-        int j = 0;
 
-        while (j < nums.length) {
-            if (nums[i] == nums[j]) {
-                count++;
-                j++;
-            } else {
-                if (count > maxCount) {
-                    maxCount = count;
-                    max = nums[i];
-                }
-                i = j;
-                count = 1;
-                j++;
-            }
+        // method 1
+        // Arrays.sort(nums);
+        // int count = 0;
+        // int maxCount = 0;
+        // int max = nums[0];
+        // int i = 0;
+        // int j = 0;
+
+        // while (j < nums.length) {
+        //     if (nums[i] == nums[j]) {
+        //         count++;
+        //         j++;
+        //     } else {
+        //         if (count > maxCount) {
+        //             maxCount = count;
+        //             max = nums[i];
+        //         }
+        //         i = j;
+        //         count = 1;
+        //         j++;
+        //     }
             
-        }
+        // }
 
-        if (count > maxCount) {
-            max = nums[i];
-        }
+        // if (count > maxCount) {
+        //     max = nums[i];
+        // }
 
-        return max;
+        // return max;
+
+
+
+        // method 2
+        int freq=0, ans=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(nums[i]==ans){
+                freq++;
+            } else{
+                freq--;
+            }
+        }
+        return ans;
     }
 }
