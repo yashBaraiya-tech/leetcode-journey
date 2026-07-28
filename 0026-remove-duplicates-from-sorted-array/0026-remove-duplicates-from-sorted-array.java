85ae0cb93c21fe1b48bbfbd0db2ecd8bd8bbbc1c
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-07-28 20:21:00 +0530

    Time: 1 ms (77.5%), Space: 46.8 MB (24.73%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0, j = 1;
+        while (j < nums.length) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+            j++;
+        }
+        return i + 1;
+    }
+}
