f3e7245dc5530b79bb0bbcf6af21b5fb5a2c7976
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-08-15 00:39:52 +0530

    Time: 1 ms (97.57%), Space: 47.1 MB (94.09%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -2,46 +2,46 @@ class Solution {
     public int pivotIndex(int[] nums) {
 
         // method 1
-        int j = 0;
-        int n = nums.length;
+        // int j = 0;
+        // int n = nums.length;
 
-        while (j < n ) {
-            int leftSum = 0;
-            int rightSum = 0;
-            for (int i = 0; i < j; i++) {
-                leftSum += nums[i];
-            }
-            for (int i = j + 1; i < n; i++) {
-                rightSum += nums[i];
-            }
+        // while (j < n ) {
+        //     int leftSum = 0;
+        //     int rightSum = 0;
+        //     for (int i = 0; i < j; i++) {
+        //         leftSum += nums[i];
+        //     }
+        //     for (int i = j + 1; i < n; i++) {
+        //         rightSum += nums[i];
+        //     }
 
-            if (leftSum == rightSum)
-                return j;
+        //     if (leftSum == rightSum)
+        //         return j;
 
-            j++;
-        }
-        return -1;
+        //     j++;
+        // }
+        // return -1;
 
         // method 2
-        // int n = nums.length;
-        // int leftSum[] = new int[n];
-        // int rightSum[] = new int[n];
+        int n = nums.length;
+        int leftSum[] = new int[n];
+        int rightSum[] = new int[n];
 
-        // leftSum[0] = nums[0];
-        // for (int i = 1; i < n; i++) {
-        //     leftSum[i] = nums[i] + leftSum[i - 1];
-        // }
+        leftSum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = nums[i] + leftSum[i - 1];
+        }
 
-        // rightSum[n - 1] = nums[n - 1];
-        // for (int i = n - 2; i >= 0; i--) {
-        //     rightSum[i] = nums[i] + rightSum[i + 1];
-        // }
+        rightSum[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] = nums[i] + rightSum[i + 1];
+        }
 
-        // for (int i = 0; i < n; i++) {
-        //     if (leftSum[i] == rightSum[i])
-        //         return i;
-        // }
+        for (int i = 0; i < n; i++) {
+            if (leftSum[i] == rightSum[i])
+                return i;
+        }
 
-        // return -1;
+        return -1;
     }
 }
