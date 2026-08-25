cbf36d36e5f8fd25859f39ed885fdf4f0334e0b9
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-08-25 20:03:51 +0530

    Time: 2 ms (68.2%), Space: 45.5 MB (26.41%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,20 +1,36 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        boolean flag = true;
-        int i = 0;
-        int j = 1;
-        int target = k * j;
 
-        while (flag && i < nums.length) {
-            if (nums[i] == target) {
-                j++;
-                i = 0;
-                target = k * j;
-            } else {
-                i++;
-            }
+        // my approch
+        // boolean flag = true;
+        // int i = 0;
+        // int j = 1;
+        // int target = k * j;
+        // while (flag && i < nums.length) {
+        //     if (nums[i] == target) {
+        //         j++;
+        //         i = 0;
+        //         target = k * j;
+        //     } else {
+        //         i++;
+        //     }
+        // }
+        // return target;
+
+        // optimal approch
+        Set<Integer> set = new HashSet<>();
+
+        for (int e : nums) {
+            set.add(e);
+        }
+
+        int target = k;
+
+        while (set.contains(target)) {
+            target += k;
         }
 
         return target;
+
     }
 }
