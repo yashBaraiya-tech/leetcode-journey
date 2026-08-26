d122acbb4b389dbc0f60fad69fe4e9764da8f31d
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-08-27 01:19:32 +0530

    Time: 1 ms (86.51%), Space: 44.2 MB (57.36%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int i = 0;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+
+        solve(nums, i, output, ans);
+
+        return ans;
+    }
+
+    static void solve(int[] nums, int i, List<Integer> output, List<List<Integer>> ans) {
+
+        if (i >= nums.length) {
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+
+        int currValue = nums[i];
+
+        // include
+        output.add(currValue);
+        solve(nums, i + 1, output, ans);
+
+        // backtraack
+        output.remove(output.size() - 1);
+
+        // exclude
+        solve(nums, i + 1, output, ans);
+
+    }
+}
