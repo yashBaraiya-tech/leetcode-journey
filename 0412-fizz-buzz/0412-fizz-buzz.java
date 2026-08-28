634866fd80f9f6053d19797a51ac3fd18e5b6936
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-08-29 00:47:06 +0530

    Time: 6 ms (1.09%), Space: 46.6 MB (83.8%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -3,15 +3,15 @@ class Solution {
         List<String> ans = new ArrayList<>();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                ans.add("FizzBuzz");
-            } else if (i % 3 == 0) {
-                ans.add("Fizz");
-            } else if (i % 5 == 0) {
-                ans.add("Buzz");
-            } else {
-                ans.add(String.valueOf(i));
-            }
+            String s = "";
+
+            if (i % 3 == 0)
+                s += "Fizz";
+            if (i % 5 == 0)
+                s += "Buzz";
+            if (s.isEmpty())
+                s += String.valueOf(i);
+            ans.add(s);
         }
 
         return ans;
