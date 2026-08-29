3c468a51fc1383532b9525296765e5c1046315b5
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-08-29 23:17:38 +0530

    Time: 1 ms (95.47%), Space: 43.6 MB (34.32%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,28 +1,25 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-         List<List<Integer>> arr = new ArrayList<>();
+        List<List<Integer>> arr = new ArrayList<>();
 
-         // create triangle like 2D arraylist
-        for(int i=0;i<numRows;i++){
-            ArrayList<Integer> a = new ArrayList<>(); // khali
-            for(int j=0;j<=i;j++){
-                a.add(0);
+
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> a = new ArrayList<>(); 
+            for (int j = 0; j <= i; j++) {
+
+                if (j == 0 || j == i)
+                    a.add(1);
+                else {
+                    int up = arr.get(i - 1).get(j);
+                    int upLeft = arr.get(i - 1).get(j - 1);
+                    a.add(up + upLeft);
+                }
+               
             }
             arr.add(a);
         }
+
         
-        // fill the values of pascal triangle
-        for(int i=0;i<numRows;i++){
-            for(int j=0;j<=i;j++){
-                if(j==0 || j==i)
-                    arr.get(i).set(j,1);
-                else{
-                    int up = arr.get(i-1).get(j);
-                    int upLeft = arr.get(i-1).get(j-1);
-                    arr.get(i).set(j,up+upLeft);
-                }
-            }
-        }
 
         return arr;
     }
