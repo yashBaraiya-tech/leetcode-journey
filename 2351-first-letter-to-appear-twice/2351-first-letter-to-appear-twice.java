07f2d1cff2736b10d874abbcdde7291b575b1fea
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-08-29 21:27:54 +0530

    Time: 0 ms (100%), Space: 42.9 MB (34.72%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,16 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        Set<Character> set = new HashSet<>();
+
+        for (char ch : s.toCharArray()) {
+            if (set.contains(ch)) {
+                return ch;
+            } else {
+                set.add(ch);
+            }
+        }
+
+        return 'a';
+
+    }
+}
