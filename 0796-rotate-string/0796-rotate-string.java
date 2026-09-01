a7535b16983bffc5e1d8d3bc62348a48921286ab
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-09-01 12:40:41 +0530

    Time: 2 ms (64.85%), Space: 43.7 MB (15.94%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+
+        if (s.length() != goal.length())
+            return false;
+
         String concat = goal + goal;
 
         return concat.contains(s);
