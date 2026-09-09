4feb19b8ce36c87e7d887f60b03a444cfefc991f
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-09-09 10:27:32 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0;
+
+        for (long x = 1000; x <= n; x *= 1000) {
+            ans += n - x + 1;
+
+            if (x > n / 1000)
+                break;
+        }
+
+        return ans;
+
+    }
+}
