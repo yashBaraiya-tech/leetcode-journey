680c314346fab88fa1b215b1d7077b460706d221
YASH BARAIYA <yashbaraiyawork3011@gmail.com>
2026-09-15 02:10:50 +0530

    Time: 10 ms (92.49%), Space: 47.1 MB (45.71%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -26,15 +26,20 @@ class Solution {
 
     static int findCount(int[] nums, int mid) {
         int n = nums.length;
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            int j = i + 1;
-            while (j < n && nums[j] - nums[i] <= mid) {
-                j++;
+
+        int i = 0;
+        int j = 1;
+        int pairCount = 0;
+
+        while (j < n) {
+
+            while (nums[j] - nums[i] > mid) {
+                i++;
             }
-            count += j - i - 1;
+            pairCount += (j - i);
+            j++;
         }
 
-        return count;
+        return pairCount;
     }
 }
